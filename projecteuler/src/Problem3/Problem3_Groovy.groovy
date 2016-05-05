8a3549d362e7b77051b9999264d70589c66a3cca
Uladzislau Kaminski <Uladzislau_Kaminski@epam.com>
2016-05-05 09:49:17 +0300

    Problem 3 is solved

--- projecteuler/src/Problem3/Problem3_Groovy.groovy
+++ projecteuler/src/Problem3/Problem3_Groovy.groovy
@@ -2,31 +2,29 @@ package Problem3
 
 class Problem3_Groovy {
     public static void main(String[] args) {
-        def number = 600851475143
-        def value = number - 1
-        def maxFactor = 0
-        while (maxFactor == 0){
-            if (number % value == 0 && isPrime(value)){
-                maxFactor = value;
-            }
-            if(value % 1000000000 == 0){
-                print value
-            }
-            --value
-        }
-        print(value)
-        print isPrime(number)
-    }
-
+		def number = 600851475143
+		def value = 1
+		def maxFactor = 0
+		while (number != 1){
+			++value
+			if (number % value == 0 && isPrime(value)){
+				number = Long.valueOf((number/value).toString())
+				maxFactor = value
+				println value
+				value = 1
+			}
+		}
+	}
+	
     def static isPrime(long number){
-        long i = 2
-        def flag = true;
-        while(i**2 <= number && flag){
-            if (number % i == 0){
-                flag = false
-            }
-            ++i;
-        }
-        flag
-    }
+		long i = 2
+		def flag = true;
+		while(i**2 <= number && flag){
+			if (number % i == 0){
+				flag = false
+			}
+			++i;
+		}
+		flag
+	}
 }
