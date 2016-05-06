3f10b532dd6aebbf3a40389c25153fe179160182
kampone <vladislavkaminski2806@gmail.com>
2016-05-07 02:12:46 +0300

    Problem 7 is solved

--- projecteuler/src/Problem3/Problem3_Groovy.groovy
+++ projecteuler/src/Problem3/Problem3_Groovy.groovy
@@ -2,29 +2,33 @@ package Problem3
 
 class Problem3_Groovy {
     public static void main(String[] args) {
-		def number = 600851475143
-		def value = 1
-		def maxFactor = 0
-		while (number != 1){
-			++value
-			if (number % value == 0 && isPrime(value)){
-				number = Long.valueOf((number/value).toString())
-				maxFactor = value
-				println value
-				value = 1
-			}
-		}
-	}
-	
-    def static isPrime(long number){
-		long i = 2
-		def flag = true;
-		while(i**2 <= number && flag){
-			if (number % i == 0){
-				flag = false
-			}
-			++i;
-		}
-		flag
-	}
+        def number = 600851475143
+        def value = 1
+        def maxFactor = 0
+        while (number != 1) {
+            ++value
+            if (number % value == 0 && isPrime(value)) {
+                number = Long.valueOf((number / value).toString())
+                maxFactor = value
+                println value
+                value = 1
+            }
+        }
+    }
+
+    def static isPrime(long number) {
+        if (number == 1L) {
+            false
+        } else {
+            long i = 2
+            def flag = true;
+            while (i**2 <= number && flag) {
+                if (number % i == 0) {
+                    flag = false
+                }
+                ++i;
+            }
+            flag
+        }
+    }
 }
---
+++ projecteuler/src/Problem7/Problem7_Groovy.groovy
@@ -0,0 +1,22 @@
+package Problem7
+
+import Problem3.Problem3_Groovy
+
+
+class Problem7_Groovy {
+    def static findPrimeNumberWithNumber(long n){
+        def numberOfPrimeNumber = 0
+        def temp = 0
+        while (numberOfPrimeNumber != n){
+            temp++
+            if (Problem3_Groovy.isPrime(temp)){
+                ++numberOfPrimeNumber
+            }
+        }
+        temp
+    }
+
+    public static void main(String[] args) {
+        print findPrimeNumberWithNumber(10001)
+    }
+}
