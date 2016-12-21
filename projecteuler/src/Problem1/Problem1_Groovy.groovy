1dad8e657c2b5021797dc834448b918752ed9965
kampone <vladislavkaminski2806@gmail.com>
2016-12-22 01:31:03 +0300

    Groovy 1 Problem update with Java Streams

--- projecteuler/src/Problem1/Problem1_Groovy.groovy
+++ projecteuler/src/Problem1/Problem1_Groovy.groovy
@@ -1,14 +1,21 @@
 package Problem1
 
+import java.util.stream.Collectors
+
+import static java.util.stream.Collectors.toList
+
 
 class Problem1_Groovy {
     static void main(String[] args) {
-        print findNumberOfIntegersThatMultipliesOfAorB(1000, 3, 5)
+        print findNumberOfIntegersMultipliesMultipliers((1..<1000), [3, 5])
     }
 
-    static findNumberOfIntegersThatMultipliesOfAorB(int border, int a, int b){
-        (1..<border).findAll {
-            !(it % a) || !(it % b)
-        }.sum()
+    static findNumberOfIntegersMultipliesMultipliers(IntRange range, List<Integer> multipliers){
+        range.stream().filter{
+            number ->
+                multipliers.stream().map{
+                    !(number % it)
+                }.any {it}
+        }.collect(toList()).sum()
     }
 }
